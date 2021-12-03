47edb06d6421b785cad8e6161c6f8e383f61a948
Robbe Van der Gucht <rvandergucht@myforce.be>
2021-12-03 15:34:26 +0100

    add TODO improve file upload

--- Create-Notification.ps1
+++ Create-Notification.ps1
@@ -42,6 +42,9 @@ Function New-Notification
     Invoke-RestMethod -Uri $Url -Method POST -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $body -Verbose
 }
 
+## TODO: Improve file upload
+# https://get-powershellblog.blogspot.com/2017/09/multipartform-data-support-for-invoke.html
+
 ##### Use this module to notify with a file upload
 # Import-Module ./CreateNotification.ps1
 # New-Notification -description "something went wrong" -system "the mainframe" -customer_guid "a2ccb709-c8f3-4df7-b5f5-f8492bb65de8" -url "http://192.168.0.148:8000/notify" -attachment_path "/mnt/Create-Notification.ps1"
