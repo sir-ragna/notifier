f933ad6fc3a6849798425cb0165ec095bc1a36a7
Robbe Van der Gucht <rvandergucht@myforce.be>
2021-12-14 10:11:03 +0100

    change login redirect

--- notifier/settings.py
+++ notifier/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = 'nginx/static/' # static files to nginx/static/ when executing:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect after login
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
