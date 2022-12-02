cdf71a5b0aeec845bdf685d02698321d78347187
Robbe Van der Gucht <sir.ragna@gmail.com>
2022-12-02 22:35:19 +0100

    esthetic spacing of command

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export SECRET_KEY='django-insecure-a62c754d-d754-4c40-a81a-73e6bff62ac6'
 export TIME_ZONE='CET'
 
 [[ -d attachments ]] || mkdir attachments 2>/dev/null
-[[ -d database ]] || mkdir database 2>/dev/null
+[[ -d database    ]] || mkdir database    2>/dev/null
 
 python manage.py runserver 0.0.0.0:8000
 
